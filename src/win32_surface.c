00fa515e09d016031d2c68c5a9244482b377f8e3
Victor Sarkisov <68136358+kelestial@users.noreply.github.com>
2021-03-01 00:37:01 -0500

    removed glfw dependency

--- src/win32_surface.c
+++ src/win32_surface.c
@@ -22,6 +22,7 @@
 
 #include <cwrenity.h>
 #include <windows.h>
+#include <GL/GL.h>
 
 #include "win32_surface.h"
 
@@ -35,6 +36,36 @@ LRESULT CALLBACK win32_callback(HWND window, UINT msg, WPARAM wParam, LPARAM lPa
 		//EVENT: window creation
 		case WM_CREATE:
 		{
+			PIXELFORMATDESCRIPTOR pfd =
+		{
+			sizeof(PIXELFORMATDESCRIPTOR),
+			1,
+			PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,    //Flags
+			PFD_TYPE_RGBA,        // The kind of framebuffer. RGBA or palette.
+			32,                   // Colordepth of the framebuffer.
+			0, 0, 0, 0, 0, 0,
+			0,
+			0,
+			0,
+			0, 0, 0, 0,
+			24,                   // Number of bits for the depthbuffer
+			8,                    // Number of bits for the stencilbuffer
+			0,                    // Number of Aux buffers in the framebuffer.
+			PFD_MAIN_PLANE,
+			0,
+			0, 0, 0
+		};
+
+		HDC ourWindowHandleToDeviceContext = GetDC(w32_handle);
+
+		int  letWindowsChooseThisPixelFormat;
+		letWindowsChooseThisPixelFormat = ChoosePixelFormat(ourWindowHandleToDeviceContext, &pfd); 
+		SetPixelFormat(ourWindowHandleToDeviceContext,letWindowsChooseThisPixelFormat, &pfd);
+
+		HGLRC ourOpenGLRenderingContext = wglCreateContext(ourWindowHandleToDeviceContext);
+		wglMakeCurrent (ourWindowHandleToDeviceContext, ourOpenGLRenderingContext);
+
+		MessageBoxA(0,(char*)glGetString(GL_VERSION), "OPENGL VERSION",0);
 			win_active = true;
 			break;
 		}
