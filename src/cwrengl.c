f538539acaab2704d5bee4e3a7846011b64849fc
Victor Sarkisov <68136358+kelestial@users.noreply.github.com>
2021-03-14 15:33:07 -0400

    removed cwrengl testing

--- src/cwrengl.c
+++ src/cwrengl.c
@@ -27,39 +27,7 @@
 #include <glad/glad.h>
 
 #define GLCALL(x) gl_clear_errors(); x; gl_poll_errors();
-
-static vert_array_t VAO;
-static vert_buffer_t *VBO;
-static vert_buffer_t *EBO;
-static shader_t SHADER;
-
-static const char *V_SRC = "#version 330 core\n"
-	"layout (location = 0) in vec3 aPos;\n"
-	"void main()\n"
-	"{\n"
-	"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-	"}\0";
-
-static const char *F_SRC = "#version 330 core\n"
-	"out vec4 FragColor;\n"
-	"void main()\n"
-	"{\n"
-		"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-	"}\0";
-
-static float VERTICES[8] = 
-{
-	 0.5f,  0.5f,
-	 0.5f, -0.5f,
-	-0.5f, -0.5f,
-	-0.5f,  0.5f
-};
-
-static unsigned int INDICIES[6] = 
-{
-	0, 1, 3,
-	1, 2, 3
-}; 
+ 
 
 static void gl_clear_errors()
 {
@@ -180,38 +148,4 @@ void cgl_enable_shader(shader_t shader)
 void cgl_dispose_shader(shader_t shader)
 {
 	GLCALL(glDeleteProgram(shader));
-}
-
-void cgl_prepare_test()
-{
-	VAO = cgl_gen_vertex_array();
-	VBO = cgl_gen_buffer(CGL_ARRAY_BUFFER);
-	EBO = cgl_gen_buffer(CGL_ELEMENT_ARRAY_BUFFER);
-
-	cgl_bind_vertex_array(VAO);
-
-	cgl_bind_buffer(EBO);
-	cgl_buffer_data(EBO, sizeof(INDICIES), INDICIES, CGL_STATIC_DRAW);
-
-	cgl_bind_buffer(VBO);
-	cgl_buffer_data(VBO, sizeof(VERTICES), VERTICES, CGL_STATIC_DRAW);
-	cgl_buffer_attrib(VBO, 0, 2, CGL_FLOAT, 2 * sizeof(float), 0);
-
-	cgl_unbind_vertex_array();
-
-	SHADER = cgl_create_shader(V_SRC, F_SRC);
-}
-
-void cgl_render_test()
-{
-	cgl_bind_vertex_array(VAO);
-	cgl_enable_shader(SHADER);
-
-	cgl_draw_elements(CGL_TRIANGLES, 6, CGL_UNSIGNED_INT, 0);
-}
-
-void cgl_cleanup_test()
-{
-	cgl_dispose_buffer(VBO);
-	cgl_dispose_shader(SHADER);
 }
