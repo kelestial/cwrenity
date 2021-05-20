0b424ee226d4ad18581431c9037aa77aac913ad9
Victor Sarkisov <68136358+kelestial@users.noreply.github.com>
2021-05-20 01:06:13 -0400

    updated background colour

--- example/src/app.h
+++ example/src/app.h
@@ -55,7 +55,7 @@ void update_game_app()
 
 void render_game_app()
 {
-	cgl_clear_colour(1.0f, 1.0f, 1.0f, 1.0f);
+	cgl_clear_colour(0.4f, 0.4f, 0.6f, 1.0f);
 
 	txr_box = cgl_create_texture();
 	cgl_bind_texture(txr_box, 0);
