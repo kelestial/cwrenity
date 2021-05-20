434746b372d126394c18e210efb81815d9358d1e
Victor Sarkisov <68136358+kelestial@users.noreply.github.com>
2021-05-19 23:50:30 -0400

    Implementing shaders

--- example/src/app.h
+++ example/src/app.h
@@ -4,20 +4,23 @@
 #ifndef _APP_H_
 #define _APP_H_
 
-static vert_array_t VAO;
-static vert_buffer_t *VBO;
-static vert_buffer_t *EBO;
-static shader_t SHADER;
+static cgl_vert_array_t va_array;
 
-static float VERTICES[8] = 
+static cgl_vert_buffer_t *vb_ebo;
+static cgl_vert_buffer_t *vb_pos;
+
+static cgl_shader_t s_shader;
+static cgl_texture_t *txr_box;
+
+static float data_vert[16] = 
 {
-	 0.5f,  0.5f,
-	 0.5f, -0.5f,
-	-0.5f, -0.5f,
-	-0.5f,  0.5f
+	 0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f,  1.0f, 0.0f,
+	-0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f,  0.0f, 1.0f
 };
 
-static unsigned int INDICIES[6] = 
+static unsigned int data_ind[6] = 
 {
 	0, 1, 3,
 	1, 2, 3
@@ -25,22 +28,24 @@ static unsigned int INDICIES[6] =
 
 void init_game_app()
 {
-	VAO = cgl_gen_vertex_array();
-	VBO = cgl_gen_buffer(CGL_ARRAY_BUFFER);
-	EBO = cgl_gen_buffer(CGL_ELEMENT_ARRAY_BUFFER);
+	va_array = cgl_gen_vertex_array();
+	vb_pos = cgl_gen_buffer(CGL_ARRAY_BUFFER);
+	vb_ebo = cgl_gen_buffer(CGL_ELEMENT_ARRAY_BUFFER);
 
-	cgl_bind_vertex_array(VAO);
+	s_shader = cgl_create_shader(cw_load_file("resources/shaders/purple.vert"), cw_load_file("resources/shaders/purple.frag"));
 
-	cgl_bind_buffer(EBO);
-	cgl_buffer_data(EBO, sizeof(INDICIES), INDICIES, CGL_STATIC_DRAW);
+	cgl_bind_vertex_array(va_array);
 
-	cgl_bind_buffer(VBO);
-	cgl_buffer_data(VBO, sizeof(VERTICES), VERTICES, CGL_STATIC_DRAW);
-	cgl_buffer_attrib(VBO, 0, 2, CGL_FLOAT, 2 * sizeof(float), 0);
+	cgl_bind_buffer(vb_ebo);
+	cgl_buffer_data(vb_ebo, sizeof(data_ind), data_ind, CGL_STATIC_DRAW);
+
+	cgl_bind_buffer(vb_pos);
+	cgl_buffer_data(vb_pos, sizeof(data_vert), data_vert, CGL_STATIC_DRAW);
+
+	cgl_buffer_attrib(0, 2, CGL_FLOAT, 4 * sizeof(float), 0);
+	cgl_buffer_attrib(1, 2, CGL_FLOAT, 4 * sizeof(float), 2 * sizeof(float));
 
 	cgl_unbind_vertex_array();
-
-	SHADER = cgl_create_shader(cw_load_file("resources/shaders/purple.vert"), cw_load_file("resources/shaders/purple.frag"));
 }
 
 void update_game_app()
@@ -52,16 +57,18 @@ void render_game_app()
 {
 	cgl_clear_colour(0.0f, 0.0f, 0.0f, 1.0f);
 
-	cgl_bind_vertex_array(VAO);
-	cgl_enable_shader(SHADER);
+	//txr_box = cgl_create_texture();
+	//cgl_bind_texture(txr_box, 0);
+	cgl_bind_vertex_array(va_array);
+	cgl_enable_shader(s_shader);
 	cgl_draw_elements(CGL_TRIANGLES, 6, CGL_UNSIGNED_INT, 0);
 }
 
 void terminate_game_app()
 {
-	cw_log_message("Terminated App", LOG_WARNING);
-	cgl_dispose_buffer(VBO);
-	cgl_dispose_shader(SHADER);
+	cgl_dispose_buffer(vb_ebo);
+	cgl_dispose_buffer(vb_pos);
+	cgl_dispose_shader(s_shader);
 }
 
 #endif
