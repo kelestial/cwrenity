acc6ae78570d2b444adc0fc2dad8fa411d0c0b8b
Victor Sarkisov <68136358+kelestial@users.noreply.github.com>
2021-05-19 22:43:20 -0400

    working on shader/texture opengl functions

--- include/cwrengl.h
+++ include/cwrengl.h
@@ -62,6 +62,52 @@
 #define CGL_FRAGMENT_SHADER 0x8B30
 #define CGL_VERTEX_SHADER 0x8B31
 
+#define CGL_CLAMP_TO_EDGE 0x812F
+
+#define CGL_RGBA 0x1908
+
+#define CGL_TEXTURE0 0x84C0
+#define CGL_TEXTURE1 0x84C1
+#define CGL_TEXTURE2 0x84C2
+#define CGL_TEXTURE3 0x84C3
+#define CGL_TEXTURE4 0x84C4
+#define CGL_TEXTURE5 0x84C5
+#define CGL_TEXTURE6 0x84C6
+#define CGL_TEXTURE7 0x84C7
+#define CGL_TEXTURE8 0x84C8
+#define CGL_TEXTURE9 0x84C9
+#define CGL_TEXTURE10 0x84CA
+#define CGL_TEXTURE11 0x84CB
+#define CGL_TEXTURE12 0x84CC
+#define CGL_TEXTURE13 0x84CD
+#define CGL_TEXTURE14 0x84CE
+#define CGL_TEXTURE15 0x84CF
+#define CGL_TEXTURE16 0x84D0
+#define CGL_TEXTURE17 0x84D1
+#define CGL_TEXTURE18 0x84D2
+#define CGL_TEXTURE19 0x84D3
+#define CGL_TEXTURE20 0x84D4
+#define CGL_TEXTURE21 0x84D5
+#define CGL_TEXTURE22 0x84D6
+#define CGL_TEXTURE23 0x84D7
+#define CGL_TEXTURE24 0x84D8
+#define CGL_TEXTURE25 0x84D9
+#define CGL_TEXTURE26 0x84DA
+#define CGL_TEXTURE27 0x84DB
+#define CGL_TEXTURE28 0x84DC
+#define CGL_TEXTURE29 0x84DD
+#define CGL_TEXTURE30 0x84DE
+#define CGL_TEXTURE31 0x84DF
+
+#define CGL_TEXTURE_2D 0x0DE1
+#define CGL_TEXTURE_MAG_FILTER 0x2800
+#define CGL_TEXTURE_MIN_FILTER 0x2801
+#define CGL_TEXTURE_WRAP_S 0x2802
+#define CGL_TEXTURE_WRAP_T 0x2803
+
+#define CGL_NEAREST 0x2600
+#define CGL_LINEAR 0x2601
+
 #define CGL_NO_ERROR 0
 
 /*
@@ -81,10 +127,10 @@ CWRENGL_API void cgl_set_viewport(int width, int height);
 #############################################
 */
 
-typedef unsigned int vert_array_t;
+typedef unsigned int cgl_vert_array_t;
 
-CWRENGL_API vert_array_t cgl_gen_vertex_array();
-CWRENGL_API void cgl_bind_vertex_array(vert_array_t va);
+CWRENGL_API cgl_vert_array_t cgl_gen_vertex_array();
+CWRENGL_API void cgl_bind_vertex_array(cgl_vert_array_t va);
 CWRENGL_API void cgl_unbind_vertex_array();
 
 /*
@@ -93,17 +139,17 @@ CWRENGL_API void cgl_unbind_vertex_array();
 #############################################
 */
 
-typedef struct vert_buffer_t
+typedef struct cgl_vert_buffer_t
 {
 	unsigned int id, type;
-} vert_buffer_t;
+} cgl_vert_buffer_t;
 
-CWRENGL_API vert_buffer_t *cgl_gen_buffer(unsigned int type);
-CWRENGL_API void cgl_bind_buffer(vert_buffer_t *vb);
-CWRENGL_API void cgl_unbind_buffer(vert_buffer_t *vb);
-CWRENGL_API void cgl_buffer_data(vert_buffer_t *vb, unsigned int size, void *data, unsigned int draw_type);
-CWRENGL_API void cgl_buffer_attrib(vert_buffer_t *vb, unsigned int index, unsigned int size, unsigned int type, unsigned int stride, int pointer);
-CWRENGL_API void cgl_dispose_buffer(vert_buffer_t *vb);
+CWRENGL_API cgl_vert_buffer_t *cgl_gen_buffer(unsigned int type);
+CWRENGL_API void cgl_bind_buffer(cgl_vert_buffer_t *vb);
+CWRENGL_API void cgl_unbind_buffer(cgl_vert_buffer_t *vb);
+CWRENGL_API void cgl_buffer_data(cgl_vert_buffer_t *vb, unsigned int size, void *data, unsigned int draw_type);
+CWRENGL_API void cgl_buffer_attrib(cgl_vert_buffer_t *vb, unsigned int index, unsigned int size, unsigned int type, unsigned int stride, int pointer);
+CWRENGL_API void cgl_dispose_buffer(cgl_vert_buffer_t *vb);
 
 /*
 #############################################
@@ -111,11 +157,28 @@ CWRENGL_API void cgl_dispose_buffer(vert_buffer_t *vb);
 #############################################
 */
 
-typedef unsigned int shader_t;
+typedef unsigned int cgl_shader_t;
 
-CWRENGL_API shader_t cgl_create_shader(const char *vert, const char *frag);
-CWRENGL_API void cgl_enable_shader(shader_t shader);
-CWRENGL_API void cgl_dispose_shader(shader_t shader);
-CWRENGL_API void cgl_shader_uniform_4f(shader_t shader, const char *uniform, float f1, float f2, float f3, float f4);
+CWRENGL_API cgl_shader_t cgl_create_shader(const char *vert, const char *frag);
+CWRENGL_API void cgl_enable_shader(cgl_shader_t shader);
+CWRENGL_API void cgl_dispose_shader(cgl_shader_t shader);
+CWRENGL_API void cgl_shader_uniform_4f(cgl_shader_t shader, const char *uniform, float f1, float f2, float f3, float f4);
+
+/*
+#############################################
+#                 TEXTURES                  #
+#############################################
+*/
+
+typedef struct cgl_texture_t
+{
+	unsigned int id; 
+} cgl_texture_t;
+
+CWRENGL_API cgl_texture_t *cgl_create_texture();
+CWRENGL_API void cgl_dispose_texture(cgl_texture_t *texture);
+CWRENGL_API void cgl_bind_texture(cgl_texture_t *texture, unsigned int slot);
+CWRENGL_API void cgl_unbind_texture();
+
 
 #endif
--- src/cwrengl.c
+++ src/cwrengl.c
@@ -26,6 +26,9 @@
 
 #include <glad/glad.h>
 
+//TODO: remove once done prototyping
+#include <stb_image.h>
+
 #define GLCALL(x) gl_clear_errors(); x; gl_poll_errors();
  
 
@@ -80,14 +83,14 @@ void cgl_set_viewport(int width, int height)
 #############################################
 */
 
-vert_array_t cgl_gen_vertex_array()
+cgl_vert_array_t cgl_gen_vertex_array()
 {
-	vert_array_t va;
+	cgl_vert_array_t va;
 	GLCALL(glGenVertexArrays(1, &va));
 	return va;
 }
 
-void cgl_bind_vertex_array(vert_array_t va)
+void cgl_bind_vertex_array(cgl_vert_array_t va)
 {
 	GLCALL(glBindVertexArray(va));
 }
@@ -103,39 +106,39 @@ void cgl_unbind_vertex_array()
 #############################################
 */
 
-vert_buffer_t *cgl_gen_buffer(unsigned int type)
+cgl_vert_buffer_t *cgl_gen_buffer(unsigned int type)
 {
-	vert_buffer_t *vb;
+	cgl_vert_buffer_t *vb;
 
-	vb = malloc(sizeof(struct vert_buffer_t));
+	vb = malloc(sizeof(struct cgl_vert_buffer_t));
 	GLCALL(glGenBuffers(1, &vb->id));
 
 	vb->type = type;
 	return vb;
 }
 
-void cgl_bind_buffer(vert_buffer_t *vb)
+void cgl_bind_buffer(cgl_vert_buffer_t *vb)
 {
 	GLCALL(glBindBuffer(vb->type, vb->id));
 }
 
-void cgl_unbind_buffer(vert_buffer_t *vb)
+void cgl_unbind_buffer(cgl_vert_buffer_t *vb)
 {
 	GLCALL(glBindBuffer(vb->type, 0));
 }
 
-void cgl_buffer_data(vert_buffer_t *vb, unsigned int size, void *data, unsigned int draw_type)
+void cgl_buffer_data(cgl_vert_buffer_t *vb, unsigned int size, void *data, unsigned int draw_type)
 {
 	GLCALL(glBufferData(vb->type, size, data, draw_type));
 }
 
-void cgl_buffer_attrib(vert_buffer_t *vb, unsigned int index, unsigned int size, unsigned int type, unsigned int stride, int pointer)
+void cgl_buffer_attrib(cgl_vert_buffer_t *vb, unsigned int index, unsigned int size, unsigned int type, unsigned int stride, int pointer)
 {
 	GLCALL(glEnableVertexAttribArray(index));
 	GLCALL(glVertexAttribPointer(index, size, type, CGL_FALSE, stride, (void*)pointer));
 }
 
-void cgl_dispose_buffer(vert_buffer_t *vb)
+void cgl_dispose_buffer(cgl_vert_buffer_t *vb)
 {
 	GLCALL(glDeleteBuffers(1, &vb->id));
 	free(vb);
@@ -147,15 +150,15 @@ void cgl_dispose_buffer(vert_buffer_t *vb)
 #############################################
 */
 
-shader_t cgl_create_shader(const char *vert, const char *frag)
+cgl_shader_t cgl_create_shader(const char *vert, const char *frag)
 {
-	shader_t program = glCreateProgram();
+	cgl_shader_t program = glCreateProgram();
 
-	shader_t vs = glCreateShader(CGL_VERTEX_SHADER);
+	cgl_shader_t vs = glCreateShader(CGL_VERTEX_SHADER);
 	GLCALL(glShaderSource(vs, 1, &vert, NULL));
 	GLCALL(glCompileShader(vs));
 
-	shader_t fs = glCreateShader(CGL_FRAGMENT_SHADER);
+	cgl_shader_t fs = glCreateShader(CGL_FRAGMENT_SHADER);
 	GLCALL(glShaderSource(fs, 1, &frag, NULL));
 	GLCALL(glCompileShader(fs));
 
@@ -169,18 +172,77 @@ shader_t cgl_create_shader(const char *vert, const char *frag)
 	return program;
 }
 
-void cgl_enable_shader(shader_t shader)
+void cgl_enable_shader(cgl_shader_t shader)
 {
 	GLCALL(glUseProgram(shader));
 }
 
-void cgl_dispose_shader(shader_t shader)
+void cgl_dispose_shader(cgl_shader_t shader)
 {
 	GLCALL(glDeleteProgram(shader));
 }
 
-void cgl_shader_uniform_4f(shader_t shader, const char *uniform, float f1, float f2, float f3, float f4)
+void cgl_shader_uniform_4f(cgl_shader_t shader, const char *uniform, float f1, float f2, float f3, float f4)
 {
 	int loc = glGetUniformLocation(shader, uniform);
+
+	if (loc == -1)
+	{
+		cw_log_message("invalid uniform location!", LOG_ERROR);
+		return;
+	}
+
 	GLCALL(glUniform4f(loc, f1, f2, f3, f4));
+}
+
+/*
+#############################################
+#                 TEXTURES                  #
+#############################################
+*/
+
+cgl_texture_t *cgl_create_texture()
+{
+	cgl_texture_t *texture = malloc(sizeof(cgl_texture_t));
+
+	//TODO: remove after prototyping!!
+	stbi_set_flip_vertically_on_load(1);
+	int width, height, nrChannels;
+	unsigned char *data = stbi_load("resources/spr_bricks_1.png", &width, &height, &nrChannels, 4); 
+
+	GLCALL(glGenTextures(1, &texture->id));
+	GLCALL(glBindTexture(CGL_TEXTURE_2D, texture->id));
+	GLCALL(glTexParameteri(CGL_TEXTURE_2D, CGL_TEXTURE_MIN_FILTER, CGL_LINEAR));
+	GLCALL(glTexParameteri(CGL_TEXTURE_2D, CGL_TEXTURE_MAG_FILTER, CGL_LINEAR));
+	GLCALL(glTexParameteri(CGL_TEXTURE_2D, CGL_TEXTURE_WRAP_S, CGL_CLAMP_TO_EDGE));
+	GLCALL(glTexParameteri(CGL_TEXTURE_2D, CGL_TEXTURE_WRAP_T, CGL_CLAMP_TO_EDGE));
+
+	GLCALL(glTexImage2D(CGL_TEXTURE_2D, 0, CGL_RGBA, width, height, 0, CGL_RGBA, CGL_UNSIGNED_BYTE, data));
+
+	return texture;
+}
+
+void cgl_dispose_texture(cgl_texture_t *texture)
+{
+	//TODO: unload texture from ram, vram, delete id
+
+	GLCALL(glDeleteTextures(1, &texture->id));
+	free(texture);
+}
+
+void cgl_bind_texture(cgl_texture_t *texture, unsigned int slot)
+{
+	if (slot > 31)
+	{
+		cw_log_message(" texture slot out of bounds! defaulting to 0x0.", LOG_ERROR);\
+		slot = 0;
+	}
+
+	GLCALL(glActiveTexture(CGL_TEXTURE0 + slot));
+	GLCALL(glBindTexture(CGL_TEXTURE_2D, texture->id));
+}
+
+CWRENGL_API void cgl_unbind_texture()
+{
+	GLCALL(glBindTexture(CGL_TEXTURE_2D, 0));
 }
